599daf32128876fdaa908fb9fa49417b43e5e873
Volker Bergner <volker@bergner.one>
2019-10-06 21:27:30 +0100

    Overview implemented with static data

---
+++ src/overview.js
@@ -0,0 +1,40 @@
+
+(function(exports) {
+  function newsOverview() {
+    this.result
+  };
+  newsOverview.prototype.pull = function() {
+    var xhr = new XMLHttpRequest();
+    // xhr.open('GET', 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?from-date=2019-10-06');
+    // xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.open('GET', 'http://localhost:9292/properties')
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        // exports.newsOverview.result = xhr.responseText // JSON/parse?
+        // exports.newsOverview.result = JSON.parse(xhr.response)
+        exports.newsOverview.result = testNewsOverview.response
+        document.dispatchEvent(overviewLoaded);
+      }
+    };
+    xhr.send();
+  };
+  newsOverview.prototype.displayOverview = function() {
+    for (i=0; i< exports.newsOverview.result.results.length; i++) {
+      document.getElementById('headlines')
+      .insertAdjacentHTML('afterend', exports.newsOverview.createArticleOverview(i));
+    }
+  }
+  newsOverview.prototype.createArticleOverview = function(i) {
+    var htmlString = '<a href=#' + i + '>';
+    htmlString += '<div id= headlines_pic' + i + '><img src=' + exports.newsOverview.result.results[i].fields.thumbnail + '></div>'
+    htmlString += '<div id= headlines' + i + '>' + exports.newsOverview.result.results[i].webTitle+ '</div></br>'
+    return htmlString
+  }
+  newsOverview.prototype.test = function() {
+    exports.newsOverview.pull();
+    exports.newsOverview.displayOverview();
+  }
+  exports.newsOverview = newsOverview;
+  var overviewLoaded = new Event('build');
+  document.addEventListener('build', function () { exports.newsOverview.displayOverview() }, false);
+})(this);
---
+++ test/overview-test.js
@@ -0,0 +1,11 @@
+document.addEventListener('build', testOverview(), false);
+
+function testOverview() {
+  // if(document.getElementById('headlines9').innerHTML !== '<tr><td><a href="#notes/1">Favourite drink: sel</a><a></a></td></tr><tr><td><a href="#notes/1">Favourite drink: sel</a><a></a></td></tr>') {
+  //   console.log("Top Article not on site as expected");
+  //   console.log(document.getElementById('noteTable').innerHTML)
+  // }
+  // else { console.log("Top Article on site as expected") }
+console.log('Test run - Top Article')  
+
+};
